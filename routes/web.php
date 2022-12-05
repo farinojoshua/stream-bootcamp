2a65bf65f1c86d877db63578399ed20775d80c1f
Farino Joshua <96770446+farinojoshua@users.noreply.github.com>
2022-12-05 08:57:06 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\MovieController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index']);
+    Route::prefix('movie')->group(function () {
+        Route::get('/', [MovieController::class, 'index'])->name('admin.movie');
+        Route::get('/create', [MovieController::class, 'create'])->name('admin.movie.create');
+        Route::post('/store', [MovieController::class, 'store'])->name('admin.movie.store');
+        Route::get('/edit/{id}', [MovieController::class, 'edit'])->name('admin.movie.edit');
+        Route::put('/update/{id}', [MovieController::class, 'update'])->name('admin.movie.update');
+
+    });
+});
